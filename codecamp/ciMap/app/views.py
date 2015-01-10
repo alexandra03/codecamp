22413eb94d0674b53897fac9137532838a16bc44
Alexandra Sunderland <alexandra.sunderland@gmail.com>
2015-01-10 01:12:31 -0500

    Kids pictures fade out to show text and fade back in on mouseout

--- codecamp/ciMap/app/views.py
+++ codecamp/ciMap/app/views.py
@@ -8,7 +8,7 @@ from app.models import Child
 import json
 
 def home(request):
-	child_list = json.loads(serializers.serialize('json', Child.objects.all(), fields=('name', 'age', 'country', 'income', 'interests', 'photo')))
+	child_list = json.loads(serializers.serialize('json', Child.objects.all(), fields=('name', 'age', 'country', 'income', 'interests', 'photo', 'id')))
 	template = loader.get_template('index.html')
 	context = Context({'child_list':child_list})
 	return HttpResponse(template.render(context))
